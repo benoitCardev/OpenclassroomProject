0527cfe6b6acfa358d996253221b3f539edb7595
Benoit <b.kasperski@cardev.fr>
2021-10-28 11:24:38 +0200

    modif contact

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
 <?php
-<<<<<<< Updated upstream
-echo "hello word";
-=======
-<<<<<<< Updated upstream
-
-=======
-<<<<<<< Updated upstream
-echo "hello word";
-=======
-echo "test";
->>>>>>> Stashed changes
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+echo "hgh";
