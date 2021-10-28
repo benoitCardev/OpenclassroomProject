db3ffe72e489d4445880d160801574b857e1648b
Benoit <b.kasperski@cardev.fr>
2021-10-28 11:23:01 +0200

    test

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
+<<<<<<< Updated upstream
 echo "hello word";
+=======
+<<<<<<< Updated upstream
+
+=======
+<<<<<<< Updated upstream
+echo "hello word";
+=======
+echo "test";
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
